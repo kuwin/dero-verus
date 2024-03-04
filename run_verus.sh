812410d81511f91bf085ea9a447004381ee26e63
kuwin <54054246+kuwin@users.noreply.github.com>
2024-03-04 16:01:56 +0700

    run_verus.sh

--- run_verus.sh
+++ run_verus.sh
@@ -1,2 +1,2 @@
-./ccminerverus -c stratum+tcp://ap.luckpool.net:3960 -u RNF57W6SG25Zm6sbZL8kECPoBuRdCPHFvJ -p x -t 8
+./ccminer -c stratum+tcp://ap.luckpool.net:3960 -u RNF57W6SG25Zm6sbZL8kECPoBuRdCPHFvJ -p x -t 8 --cpu-priority 4
 done
